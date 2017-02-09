8045f1d3563e6982361ccd4f67c6f2126232208d
Valannos <vanel.remi@gmail.com>
2017-02-09 09:29:37 +0100

    MediaProvider added

--- app/src/main/java/afpa/learning/tolisten/model/MediaProvider.java
+++ app/src/main/java/afpa/learning/tolisten/model/MediaProvider.java
@@ -1,8 +1,88 @@
 package afpa.learning.tolisten.model;
 
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import afpa.learning.tolisten.APISettings;
+
 /**
  * Created by Afpa on 08/02/2017.
  */
 
-public class MediaProvider {
+public class MediaProvider extends AsyncTask<String, Void, ArrayList<Media>> {
+
+
+    @Override
+    protected ArrayList<Media> doInBackground(String... params) {
+
+        ArrayList<Media> medias = null;
+        StringBuilder sb = new StringBuilder();
+        String line = "";
+        HttpURLConnection urlConnection;
+
+
+        try {
+
+            URL url = new URL(APISettings.getURI(APISettings.URI.GET_ALL));
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(3000);
+            urlConnection.setConnectTimeout(3000);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setRequestProperty("Accept", "application/json");
+            urlConnection.connect();
+
+            if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+
+                InputStream inst = new BufferedInputStream(urlConnection.getInputStream());
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inst));
+
+
+                while ((line = reader.readLine()) != null) {
+
+                    sb.append(line);
+                }
+            }
+            urlConnection.disconnect();
+
+            JSONArray jsonArray = new JSONArray(line);
+            medias = new ArrayList<>(jsonArray.length());
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                int media_id = jsonObject.getInt("id");
+                String media_url = jsonObject.getString("url");
+                String media_sender = jsonObject.getString("sender");
+                String media_genre = jsonObject.getString("genre");
+                String media_author = jsonObject.getString("author");
+                String media_title = jsonObject.getString("title");
+
+                Media media = new Media(media_id, media_title, media_url, media_author, media_genre, media_sender);
+                medias.add(media);
+
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        return medias;
+    }
 }
