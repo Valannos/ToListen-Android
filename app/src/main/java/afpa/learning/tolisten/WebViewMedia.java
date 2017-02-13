12640f688471e1b208c68a3c8fe9fdb75774ac8f
Valannos <vanel.remi@gmail.com>
2017-02-13 17:03:09 +0100

    POST in progress

--- app/src/main/java/afpa/learning/tolisten/WebViewMedia.java
+++ app/src/main/java/afpa/learning/tolisten/WebViewMedia.java
@@ -1,6 +1,15 @@
 package afpa.learning.tolisten;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import afpa.learning.tolisten.model.Media;
 
 /**
  * Created by Afpa on 13/02/2017.
@@ -8,6 +17,46 @@ import android.app.Activity;
 
 public class WebViewMedia extends Activity {
 
+private Switch switchView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
 
+        super.onCreate(savedInstanceState);
+        Intent currentIntent = getIntent();
+        setContentView(R.layout.activity_webviewmedia);
+
+        switchView = (Switch) findViewById(R.id.switchViewState);
+
+        Media media = new Media(
+
+                (Integer) currentIntent.getExtras().get("media_id"),
+                (String) currentIntent.getExtras().get("media_title"),
+                (String) currentIntent.getExtras().get("media_url"),
+                (String) currentIntent.getExtras().get("media_author"),
+                (String) currentIntent.getExtras().get("media_genre"),
+                (String) currentIntent.getExtras().get("media_user"),
+                (Boolean) currentIntent.getExtras().get("media_isViewed")
+
+        );
+
+        WebView browser = (WebView) findViewById(R.id.webViewMedia);
+        WebSettings webSettings = browser.getSettings();
+        browser.setWebViewClient(new WebViewClient());
+        webSettings.setJavaScriptEnabled(true);
+        // browser.loadUrl("http://www.html5rocks.com/");
+
+        browser.loadUrl(media.getUrl());
+        TextView webViewTitle = (TextView) findViewById(R.id.webViewTitle);
+
+        webViewTitle.setText(media.getAuthor() + " - " + media.getTitle());
+
+        System.out.println(media.isViewed());
+
+
+        switchView.setChecked(media.isViewed());
+
+
+    }
 }
--- app/src/main/java/afpa/learning/tolisten/model/ListMediaClicked.java
+++ app/src/main/java/afpa/learning/tolisten/model/ListMediaClicked.java
@@ -1,7 +1,11 @@
 package afpa.learning.tolisten.model;
 
+import android.content.Intent;
 import android.view.View;
 import android.widget.AdapterView;
+
+import afpa.learning.tolisten.WebViewMedia;
+
 
 /**
  * Created by Afpa on 13/02/2017.
@@ -15,7 +19,17 @@ public class ListMediaClicked implements AdapterView.OnItemClickListener {
 
         System.out.println("Click Performed on: " + media.getTitle());
 
-        //Intent intent = new Intent(Activity.this,destinationActivity.class);
-        //startActivity(intent);
+        Intent intent = new Intent(parent.getContext(), WebViewMedia.class);
+        intent.putExtra("media_title", media.getTitle());
+        intent.putExtra("media_genre", media.getGenre());
+        intent.putExtra("media_author", media.getAuthor());
+        intent.putExtra("media_user", media.getUser());
+        intent.putExtra("media_url", media.getUrl());
+        intent.putExtra("media_id", media.getId());
+
+        intent.putExtra("media_isViewed", media.isViewed());
+
+
+        parent.getContext().startActivity(intent);
     }
 }
